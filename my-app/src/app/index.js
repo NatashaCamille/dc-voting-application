b3f9c9e57329388afa6dcbc762da7f44531c7f15
Natasha Camille <123318520+NatashaCamille@users.noreply.github.com>
2024-06-12 20:31:08 +0000

    Updates votung app

--- my-app/src/app/index.js
+++ my-app/src/app/index.js
@@ -5,6 +5,27 @@ export default function Home() {
 
 
     const checkIfWalletIsConnected = async () => {
-
-    }
+        try {
+            const { ethereum } = window;
+      
+            if (!ethereum) {
+              console.log("Please install metamask!!!");
+            } else {
+              // console.log("We have the ethereum object", ethereum);
+            }
+      
+            const accounts = await ethereum.request({ method: "eth_accounts" });
+            if (accounts.length) {
+              const account = accounts[0];
+              // console.log("Authorized account has found", account);
+              setCurrentAccount(account);
+              console.log("Connected");
+            } else {
+              setCurrentAccount("");
+              console.log("No authorized account has found!");
+            }
+          } catch (error) {
+            console.error(error);
+          }
+    };
 }
