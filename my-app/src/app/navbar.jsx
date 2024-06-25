96c2e42ab2b5ac270f3e871d263c2ee57bade36a
Natasha Camille <123318520+NatashaCamille@users.noreply.github.com>
2024-06-25 23:59:04 +0300

    Update navbar.jsx

---
+++ my-app/src/app/navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = ({ account, connectWallet }) => {
+
+  return (
+       <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-gray-900 to-gray-700 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <span className="font-bold text-3xl tracking-tight">
+          Web3 Voting DAPP
+          </span>
+          </div>
+          
+         
+       </nav>
+    )
+}
