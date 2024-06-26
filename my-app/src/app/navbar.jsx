94b6880d451777cb75c2a8f18c173c2a5871fbdb
Natasha Camille <123318520+NatashaCamille@users.noreply.github.com>
2024-06-26 23:21:41 +0300

    Update navbar.jsx

--- my-app/src/app/navbar.jsx
+++ my-app/src/app/navbar.jsx
@@ -7,7 +7,24 @@ const Navbar = ({ account, connectWallet }) => {
           Web3 Voting DAPP
           </span>
           </div>
-          
+            <div className="flex">
+                 {account ? (
+              <button
+                type="button"
+                className='className="bg-gradient-to-r from-blue-500 to-blue-700 text-white py-2 px-4 rounded-m font-bold'
+              >
+                {account.slice(0, 6) + '...' + account.slice(38, 42)}
+              </button>
+            ) : (
+              <button
+                type="button"
+                className='className="bg-blue-500 bg-blue-700 text-white font-bold py-2 px-4 rounded"'
+                onClick={connectWallet}
+              >
+                Connect Wallet
+              </button>
+            )}
+            </div>
          
        </nav>
     )
